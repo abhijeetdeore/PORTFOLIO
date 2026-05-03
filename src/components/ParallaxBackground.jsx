22bae1f6461d6dd325919baa54ec983780fc1f7b
abhijeetdeore <abhijee7ruia@gmail.com>
2026-05-03 18:44:36 +0530

    Fix

--- src/components/ParallaxBackground.jsx
+++ src/components/ParallaxBackground.jsx
@@ -1,30 +1,25 @@
-
 import React from 'react'
+import skyy from '../imgcomps/skyy.png'
+import boat from '../imgcomps/boat.png'
 
 const ParallaxBackground = () => {
-
   return (
     <section className='absolute inset-0 bg-black/40'>
       <div className='relative h-screen overflow-y-hidden'>
-        {/* background sky */}
         <div
           className='absolute inset-0 w-full h-screen -z-50'
           style={{
-            backgroundImage: "url(/src/imgcomps/skyy.png)",
-            backgroundPosition: "borrom",
+            backgroundImage: `url(${skyy})`,
+            backgroundPosition: "bottom",
             backgroundSize: "cover",
           }} />
-        {/* background Boat */}
         <div 
-        className='absolute inset-0 -z-40'
-        style={{
-            backgroundImage: "url(/src/imgcomps/boat.png)",
-            backgroundPosition: "borrom",
+          className='absolute inset-0 -z-40'
+          style={{
+            backgroundImage: `url(${boat})`,
+            backgroundPosition: "bottom",
             backgroundSize: "cover",
-            
           }}/>
-
-
       </div>
     </section>
   )
